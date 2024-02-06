3a522a19653313dcc025e439bd5bbc27248e7c48
Pranit Dhage <pranitdhage92@gmail.com>
2024-02-06 16:45:57 +0530

    Code Updated

--- src/main/java/com/address/service/IUserClient.java
+++ src/main/java/com/address/service/IUserClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(url = "http://localhost:8081", value = "User-Client")
-@FeignClient(name = "USER-SERVICE:8081")
+@FeignClient(name = "USER-SERVICE")
 public interface IUserClient {
 
     @GetMapping(
